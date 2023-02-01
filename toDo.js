963185a2a133aeb9e3b379f183acd793f986bf54
DevarajI2 <devaraj.venkatesan@ideas2it.com>
2023-02-01 19:48:30 +0530

    created task down button and remove task

--- toDo.js
+++ toDo.js
@@ -1,5 +1,25 @@
 function addTask() {
-    let text = document.createElement("p");
-    text.innerText = document.getElementById("inputValue").value;
+    let text = document.createElement("div");
+    text.className = "addTask";
+    textValue = text.innerHTML = document.getElementById("inputValue").value;
+    if (textValue == "") {
+        alert("Add Task is mandatory !");
+        return false;
+    }
     document.getElementById("tasks").appendChild(text);
+    let removeTask = document.createElement('input');
+    removeTask.className = "removeButton";
+    removeTask.setAttribute('type','button');
+    removeTask.setAttribute("value","Remove");
+    removeTask.setAttribute("id","removeButton");
+    removeTask.addEventListener('click',function(e) {
+          text.parentNode.removeChild(text);
+    }, false);
+    text.appendChild(removeTask);
 }
+// var i;
+//     for (i = 0; i < text.length; i++) {
+//     let span = document.createElement("span");
+//     let remove = document.createTextNode("\u00D7");
+//     span.appendChild(remove);
+//     }
