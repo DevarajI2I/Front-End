0422822b2b1446e9a79012231102dd8d69c45a62
DevarajI2 <devaraj.venkatesan@ideas2it.com>
2023-02-03 19:34:47 +0530

    created default checkbox alter css in completed task

--- toDo.js
+++ toDo.js
@@ -2,11 +2,12 @@ function addTask() {
     let text = document.createElement("div");
     text.className = "addTask";
     textValue = text.innerHTML = document.getElementById("inputValue").value;
+    
     if (textValue == "") {
         alert("Add Task is mandatory !");
         return false;
     }
-    document.getElementById("tasks").appendChild(text);
+    document.getElementById("pendingTask").appendChild(text);
 
     let removeTask = document.createElement('input');
     removeTask.className = "removeButton";
@@ -14,7 +15,10 @@ function addTask() {
     removeTask.setAttribute("value","Remove");
     removeTask.setAttribute("id","removeButton");
     removeTask.addEventListener('click',function() {
+        let warningText = "Are You Sure Delete This Task !";
+        if (confirm(warningText) == true) {
           text.parentNode.removeChild(text);
+        }
     }, false);
     text.appendChild(removeTask);
     document.getElementById("inputValue").value = "";
@@ -23,13 +27,28 @@ function addTask() {
     checkBox.setAttribute("id","checkBox");
     checkBox.type = "checkbox";
     checkBox.setAttribute("onchange","completedTask(checked,this)");
+    editTask();
     text.appendChild(checkBox);
-
 }
 function completedTask(isChecked,element) {
     if (isChecked) {
-        element.parentElement.style.backgroundColor = "lightgray";
+        let subDiv = document.createElement("div");
+        document.getElementById("completedTag").style.display = "block";
+        subDiv.className = "completedDiv";
+        subDiv = element.parentElement;
+        document.getElementById("specifyTask").appendChild(subDiv);
+        
     } else {
-        element.parentElement.style.backgroundColor = "#6dd9bb";
+        count = document.getElementById("pendingTask").childElementCount-1;
+        if (count == 0) {
+            document.getElementById("completedTag").style.display = "none";
+        }
+        document.getElementById("pendingTask").appendChild(element.parentElement);
     }
+}
+function editTask() {
+    let edit = document.createElement("input");
+    edit.setAttribute("type","button");
+    edit.appendChild(parentElement);
+
 }
